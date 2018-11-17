00b32ab656bae1b620d5349475476829a8dcb3c7
Razvan Goga <razvan.goga@gmail.com>
2018-11-17 15:16:44 +0100

    itertion 5 start

---
+++ game-of-life-5/cell.go
@@ -0,0 +1,5 @@
+package game_of_life_5
+
+func IsAlive(cell int, neighbours []int)int {
+	return -1
+}
---
+++ game-of-life-5/universe.go
@@ -0,0 +1,5 @@
+package game_of_life_5
+
+func Evolve(current [][]int) [][]int {
+	return nil
+}
