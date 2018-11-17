c418353b25a80de537064e63f9e5fe1cbe52ca6a
Razvan Goga <razvan.goga@gmail.com>
2018-11-17 15:19:55 +0100

    test passing

--- game-of-life-5/cell.go
+++ game-of-life-5/cell.go
@@ -1,5 +1,16 @@
 package game_of_life_5
 
 func IsAlive(cell int, neighbours []int)int {
+
+	sum:=0
+
+	for i:=0;i<len(neighbours); i++  {
+		sum+= neighbours[i]
+	}
+
+	if(sum < 2 && cell == 1){
+		return 0
+	}
+
 	return -1
 }
--- game-of-life-5/cell_test.go
+++ game-of-life-5/cell_test.go
@@ -1,1 +1,12 @@
 package game_of_life_5
+
+import "testing"
+
+func TestIsAlive(t *testing.T) {
+	actual:= IsAlive(1, []int {1,0,0,0,0,0,0,0})
+	expected:= 0
+
+	if actual != expected {
+		t.Errorf("Expected %v but instead got %v", expected, actual)
+	}
+}
